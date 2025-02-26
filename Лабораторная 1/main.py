33add98b41b3374848dcd1bc87b1533e3e9916d4
Малыхин Владислав Олегович <113335402+mvofus@users.noreply.github.com>
2025-02-26 13:13:49 +0300

    Исправления
    Исправления

--- Лабораторная 1/main.py
+++ Лабораторная 1/main.py
@@ -30,23 +30,48 @@ class Film:
         self.timestamp = 0  # Временная отметка воспроизведения
         self.status = "PAUSED"  # Статус воспроизведения
     def play(self) -> None:  # меняет состояние на воспроизведение
-        """ Метод воспроизведения"""
+        """ 
+        Метод воспроизведения
+        
+         Пример:
+        >>> film = Film("IT", 120, 8.8)
+        >>> film.pause()
+        """
         self.status = "PLAYING"
         ...
     def pause(self) -> None: # меняет состояние на паузу и записывает таймстемп в минутах для продолжения воспроизведения
-        """ Метод паузы."""
+        """
+        Метод паузы.
+        
+        Пример:
+        >>> film = Film("IT", 120, 8.8)
+        >>> film.pause()
+        """
         self.status = "PAUSED"
         self.timestamp = self.get_timestamp()
     def get_timestamp(self) -> int:  # возвращает int таймстемп в минутах
-        """Метод получения таймстемпа"""
+        """
+        Метод получения таймстемпа
+        
+        Пример:
+        >>> film = Film("IT", 120, 8.8)
+        >>> get_video_player_timestamp()
+        0
+        """
         timestamp = get_video_player_timestamp()
         if timestamp > self.running_time_min:
             raise ValueError("Timestamp must be less than the running_time_min")
         return timestamp
-
+    
 def get_video_player_timestamp() -> int: # Получить время воспроизведения из api видеоплеера
-    """ Метод получения отметки от апи плеера. """
-    ...
+    """
+    Метод получения отметки от апи плеера.
+    
+    Пример:
+    >>> get_video_player_timestamp()
+    0
+    """
+    return 0 # в качестве заглушки, нужно чтобы тможно было пройти тесты
 
 
 class Car:
@@ -61,21 +86,24 @@ class Car:
         :param tank_capacity: Объём бака автомобиля
         :param occupied_volume:  Объём топлива в баке
         :param consumption: Потребление топлива
+        
+        Пример:
+        >>> car = Car("RED", 100, 50, 1)
         """
         if not isinstance(color, str):
-            raise TypeError
+            raise TypeError("Invalid type of color")
         if not isinstance(tank_capacity, (int, float)):
-            raise TypeError
-        if not tank_capacity > 0:
-            raise ValueError
+            raise TypeError("Invalid type of tank capacity")
+        if tank_capacity <= 0:
+            raise ValueError("Invalid value of tank capacity")
         if not isinstance(occupied_volume, (int, float)):
-            raise TypeError
+            raise TypeError("Invalid type of occupied volume")
         if  occupied_volume < 0 or occupied_volume > tank_capacity:
-            raise ValueError
+            raise ValueError("Invalid value of occupied volume")
         if not isinstance(consumption, (int, float)):
-            raise TypeError
-        if not consumption > 0:
-            raise ValueError
+            raise TypeError("Invalid type of consumption")
+        if consumption <= 0:
+            raise ValueError("Invalid value of consumption")
         self.color = color  # Цвет
         self.tank_capacity = tank_capacity  # Объём бака
         self.occupied_volume = occupied_volume  # Объём топлива в баке
@@ -96,7 +124,13 @@ class Car:
         return self.occupied_volume - path * self.consumption
 
     def recolor(self, new_color: str) -> None:  # задаёт новый цвет машины
-        """ Метод перекраски машины. """
+        """ 
+        Метод перекраски машины. 
+        
+        Пример:
+        >>> car = Car("RED", 100, 50, 1)
+        >>> car.recolor("BLUE")
+        """
         if not isinstance(new_color, str):
             raise TypeError
         ...
@@ -111,6 +145,9 @@ class Phone:
         """
          Инициализация экземпляра класса.
         :param charge_level: Уровень заряда телефона
+        
+        Пример:
+        >>> phone = Phone(100)
         """
         self.MAX_CHARGE = 100  # уровень заряда в %
         """ Максимальный заряд телефона. """
@@ -121,18 +158,30 @@ class Phone:
         if self.MAX_CHARGE < 0 or charge_level > self.MAX_CHARGE:
             raise ValueError
         self.charge_level = charge_level
-    def charge(self, new_charge_level) -> None:
-        """ Метод заряжания телефона. """
+    def charge(self, new_charge_level: Union[int, float]) -> None:
+        """
+        Метод заряжания телефона.
+        
+        Пример:
+        >>> phone = Phone(50)
+        >>> phone.charge(90)
+        """
         if not isinstance(new_charge_level, (int, float)):
-            raise TypeError
-        if new_charge_level < self.MIN_CHARGE or new_charge_level > self.MAX_CHARGE or self.charge_level < new_charge_level:
+            raise TypeError("Invalid type of charge level")
+        if new_charge_level < self.MIN_CHARGE or new_charge_level > self.MAX_CHARGE or self.charge_level > new_charge_level:
             raise ValueError("Can not charge up to this level!")
         self.charge_level = new_charge_level
-    def discharge(self, new_charge_level) -> None:
-        """ Метод разряжания телефона. """
+    def discharge(self, new_charge_level: Union[int, float]) -> None:
+        """
+        Метод разряжания телефона.
+        
+        Пример:
+        >>> phone = Phone(50)
+        >>> phone.discharge(10)
+        """
         if not isinstance(new_charge_level, (int, float)):
-            raise TypeError
-        if new_charge_level < self.MIN_CHARGE or new_charge_level > self.MAX_CHARGE or self.charge_level > new_charge_level:
+            raise TypeError("Invalid type of charge level")
+        if new_charge_level < self.MIN_CHARGE or new_charge_level > self.MAX_CHARGE or self.charge_level < new_charge_level:
             raise ValueError("Can not charge down to this level!")
         self.charge_level = new_charge_level
 
